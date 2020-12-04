3ba0a044c680180c837a0c75450261691fbfa296
Rohin Rangnekar <31345389+rohin1994@users.noreply.github.com>
2020-12-04 14:50:44 +0530

    Reference files added

--- main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  const logo = document.querySelectorAll("#logo path");
-  for (let i = 0; i < logo.length; i++) {
-    console.log(i + " is " + logo[i].getTotalLength());
-  }
-});
